04bfff2ec87326e413ced637ca5e9a2662eba69f
Taras Zadziarnouski <taras.zadziarnouski@gmail.com>
2024-09-23 12:57:08 +0200

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "com.zadziarnouski"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
