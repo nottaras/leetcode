67d1cb29998df99e8f51657f34b975fe75587f06
Taras Zadziarnouski <taras.zadziarnouski@gmail.com>
2025-09-08 20:08:39 +0200

    Refactoring

--- src/main/java/com/zadziarnouski/leetcode/GroupAnagrams.java
+++ src/main/java/com/zadziarnouski/leetcode/GroupAnagrams.java
@@ -6,20 +6,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// TC: O(n * k log k), SC: O(n * k)
 class GroupAnagrams {
 
     static List<List<String>> groupAnagrams(String[] strs) {
-
-
-        Map<String, List<String>> result = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
             String key = new String(chars);
-            result.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
 
-        return new ArrayList<>(result.values());
+        return new ArrayList<>(map.values());
     }
 }
