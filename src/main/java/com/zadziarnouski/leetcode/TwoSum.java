d53b377d0b2970155368f59257741e04f36571de
Taras Zadziarnouski <taras.zadziarnouski@gmail.com>
2025-09-08 12:28:05 +0200

    Add new solution for 1. Two Sum

--- src/main/java/com/zadziarnouski/leetcode/TwoSum.java
+++ src/main/java/com/zadziarnouski/leetcode/TwoSum.java
@@ -1,15 +1,18 @@
 package com.zadziarnouski.leetcode;
 
+import java.util.HashMap;
+
 // TC: O(n), SC: O(n)
 class TwoSum {
-    //TODO: Consider solving this using a hash table for better performance (O(n) time)
     static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
 
         return null;
